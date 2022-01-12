1e91567906d30ac8c8452ae6c3e06e1be80583be
Florian Pfisterer <florian.pfisterer.2@gmail.com>
2022-01-12 16:05:05 +0100

    fix: UI fixes

--- mvgcli/next_departures.py
+++ mvgcli/next_departures.py
@@ -3,6 +3,10 @@ from datetime import datetime
 from mvg_api import Station
 
 
+def _get_prefix(departure) -> str:
+    return f"{departure['label']} {departure['destination']}"
+
+
 def get_next_departures(args: Namespace):
     station_name = args.start_station
     station = Station(station_name)
@@ -11,12 +15,12 @@ def get_next_departures(args: Namespace):
         print(f'Could not find station for query {station_name}.')
         return
 
+    print(f'Upcoming departures from {station.name}\n')
+
     departures = station.get_departures(timeoffset=args.offset)
-
-    print(f'Next departures from {station.name}:\n')
     dest_station = args.dest_station if args.dest_station is not None else None
-    longest_name = ''
 
+    longest_prefix = ''
     valid_departures = []
     i = 0
     while len(valid_departures) <= args.limit and i < len(departures):
@@ -26,16 +30,22 @@ def get_next_departures(args: Namespace):
         if dest_station is not None and dest_station not in departure['destination']:
             continue
  
-        longest_name = departure['destination'] if len(departure['destination']) > len(longest_name) else longest_name
+        prefix = _get_prefix(departure)
+        longest_prefix = prefix if len(prefix) > len(longest_prefix) else longest_prefix
         valid_departures.append(departure)
 
-    padding_length = len(longest_name) + 5
+    padding_length = len(longest_prefix) + 5
     valid_departures = sorted(valid_departures, key=lambda d: d['departureTimeMinutes'])
     for departure in valid_departures: 
         time = datetime.fromtimestamp(int(departure['departureTime']) / 1000).strftime('%H:%M')
+
         delay = departure['delay'] if 'delay' in departure else 0
-        padding = ' ' * (padding_length - len(departure['destination']))
+        delay = f'+{delay}' if delay >= 0 else f'{delay}'
+
+        prefix = _get_prefix(departure)
+        padding = ' ' * (padding_length - len(prefix))
 
         time_mins = departure['departureTimeMinutes']
         relative_description = f'in {time_mins} min' if time_mins >= 0 else 'now'
-        print(f"{departure['label']} to {departure['destination']}{padding}{time} +{delay} ({relative_description})")
+
+        print(f"{prefix}{padding}{time} {delay} ({relative_description})")
