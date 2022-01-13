f8d975da7ff204ab5a6f37c7a7031c6146a622bb
Florian Pfisterer <florian.pfisterer.2@gmail.com>
2022-01-13 11:22:36 +0100

    chore: prep for PyPI

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 import setuptools
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
 
 
 setuptools.setup(
-    name='mvg',
-    version='1.0',
+    name='mvg-cli',
+    version='1.0.0',
+
     author='Florian Pfisterer',
-    description='Access MVG data from your command line.',
+    author_email='florian@pfisterer.dev',
+    description='A CLI tool to get quick access to live MVG data from the command line.',
+    long_description=README,
+    long_description_type='text/markdown',
+    url='https://github.com/FlorianPfisterer/mvg-cli',
     packages=['mvgcli'],
     install_requires=[line.strip() for line in open('requirements.txt', 'r').readlines()],
     python_requires='>=3.5',
