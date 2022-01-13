702311a172643e03ade83ae29059b67026254dd0
Florian Pfisterer <florian.pfisterer.2@gmail.com>
2022-01-13 11:48:47 +0100

    chore: cleanup

--- setup.py
+++ setup.py
@@ -4,6 +4,12 @@ import pathlib
 HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
+# replace the image URL to be able to show it on PyPI
+README = README.replace(
+    '(./example.png)',
+    '(https://github.com/FlorianPfisterer/mvg-cli/raw/main/example.png)'
+)
+
 
 setuptools.setup(
     name='mvg-cli',
@@ -11,11 +17,14 @@ setuptools.setup(
 
     author='Florian Pfisterer',
     author_email='florian@pfisterer.dev',
+
     description='A CLI tool to get quick access to live MVG data from the command line.',
     long_description=README,
-    long_description_type='text/markdown',
+    long_description_content_type='text/markdown',
     url='https://github.com/FlorianPfisterer/mvg-cli',
+
     packages=['mvgcli'],
+    include_package_data=True,
     install_requires=[line.strip() for line in open('requirements.txt', 'r').readlines()],
     python_requires='>=3.5',
     entry_points={
